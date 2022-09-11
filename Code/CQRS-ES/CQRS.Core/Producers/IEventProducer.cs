17592b09d08c5797dbbbf9862e93c38318605174
Yuvaraj Kesavan <yuvaraj.kesavan@hotmail.com>
2022-09-11 16:15:49 -0500

    EventProducer Implementation

---
+++ Code/CQRS-ES/CQRS.Core/Producers/IEventProducer.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Events;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Producers
+{
+    public interface IEventProducer
+    {
+        Task ProduceAsync<T>(string topic, T @event) where T : BaseEvent;
+    }
+}
--- Code/SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
+++ Code/SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -2,6 +2,7 @@
 using CQRS.Core.Events;
 using CQRS.Core.Exceptions;
 using CQRS.Core.Infrastructure;
+using CQRS.Core.Producers;
 using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Repositories;
 using System;
@@ -16,10 +17,13 @@ namespace Post.Cmd.Infrastructure.Stores
     public class EventStore : IEventStore
     {
         private readonly IEventStoreRepository _eventStoreRepository;
+        private readonly IEventProducer _eventProducer;
+
         
-        public EventStore(IEventStoreRepository eventStoreRepository)
+        public EventStore(IEventStoreRepository eventStoreRepository, IEventProducer eventProducer)
         {
             _eventStoreRepository = eventStoreRepository;
+            _eventProducer = eventProducer;
         }
 
         public async Task<List<Guid>> GetAggregateIdsAsync()
@@ -69,6 +73,8 @@ namespace Post.Cmd.Infrastructure.Stores
 
                 await _eventStoreRepository.SaveAsync(eventModel);
 
+                var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+                await _eventProducer.ProduceAsync(topic, @event);
                 
             }
         }
