c230a9d65d7c2949ed67e2f23be4330f01b77a30
Yuvaraj Kesavan <yuvaraj.kesavan@hotmail.com>
2022-09-13 15:53:00 -0500

    Implemented NewPostController

---
+++ Code/SM-Post/Post.Cmd/Post.Cmd.Api/DTOs/NewPostResponse.cs
@@ -0,0 +1,14 @@
+﻿using Post.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Cmd.Api.DTOs
+{
+    public class NewPostResponse : BaseResponse
+    {
+        public Guid Id { get; set; }
+    }
+}
--- Code/SM-Post/Post.Query/Post.Query.Infrastructure/Repositories/PostRepository.cs
+++ Code/SM-Post/Post.Query/Post.Query.Infrastructure/Repositories/PostRepository.cs
@@ -2,11 +2,6 @@
 using Post.Query.Domain.Entities;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Post.Query.Infrastructure.Repositories
 {
